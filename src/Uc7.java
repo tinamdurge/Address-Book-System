cd0140a1d7d4344fbd63d5f4867d67873a4af034
Tinam Durge <tinamdurge02@gmail.com>
2023-12-12 15:37:50 +0530

    Added: No duplicate entry of same person

--- src/Uc7.java
+++ src/Uc7.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Uc7 {
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class Uc7 {
+
+    // To check adding new contact is already exist in addressBook
+    public static boolean check(Contacts contacts, ArrayList<Contacts> contatctsArrayList) {
+
+        boolean Exists = Stream.of(contacts).anyMatch(e -> contatctsArrayList.contains(e));
+        return Exists;
+    }
 }
