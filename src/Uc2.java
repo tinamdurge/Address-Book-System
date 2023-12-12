1154c0d86a9e376f7df4e1a2437b23fd1d897a2a
Tinam Durge <tinamdurge02@gmail.com>
2023-12-12 15:44:57 +0530

    Added: New contact to address book

--- src/Uc2.java
+++ src/Uc2.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class Uc2 {
+import java.util.Scanner;
+
+public class Uc2 {
+
+    // Use addContacts method and get input from user
+    public static void addContacts() {
+
+        Scanner sc = new Scanner(System.in);
+        Contacts contacts = new Contacts();
+
+        System.out.println("Enter First Name :");
+        String firstName = sc.next();
+        contacts.setFirstName(firstName);
+
+        System.out.println("Enter Last Name :");
+        String lastName = sc.next();
+        contacts.setLastName(lastName);
+
+        System.out.println("Enter The Address :");
+        String address = sc.next();
+        contacts.setAddress(address);
+
+        System.out.println("Enter The City :");
+        String city = sc.next();
+        contacts.setCity(city);
+
+        System.out.println("Enter The State :");
+        String state = sc.next();
+        contacts.setState(state);
+
+        System.out.println("Enter The Zip Number :");
+        int zip = sc.nextInt();
+        contacts.setZip(zip);
+
+        System.out.println("Enter Phone Number :");
+        long phoneNumber = sc.nextLong();
+        contacts.setPhoneNumber(phoneNumber);
+
+        System.out.println("Enter The Email :");
+        String email = sc.next();
+        contacts.setEmail(email);
+
+        AddressBookMainFunction.contatctsArrayList1.add(contacts); // adding contacts to the AddressBook
+    }
 }
